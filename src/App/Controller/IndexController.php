10d1918a5fbfc27e39fb00fecc6cd0c8ba7ba9b7
Bertrand <bertrand.fernandez@gmail.com>
2017-10-05 12:58:08 +0200

    Mise en place Notation
    Mise en place de la possibilité de noter et commenter les services d'un utilisateur.  Possibilité uniquement de noter. Affichage des commentaires et des notes sur la fiche annonce.

--- src/App/Controller/IndexController.php
+++ src/App/Controller/IndexController.php
@@ -10,6 +10,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 class IndexController
@@ -72,7 +75,8 @@ class IndexController
         $nomCategorieService,
         $slugService,
         $idService,
-        Application $app)
+        Application $app,
+        Request $request)
     {
 
         # Récupération de l'annonce
@@ -99,13 +103,114 @@ class IndexController
             #Je recupére geo_point_2D 
             $latitude = substr($service['geo_point_2d'], 0, strpos($service['geo_point_2d'], ','));
             $longitude = substr($service['geo_point_2d'], strpos($service['geo_point_2d'],',')+strlen(','));
+            
+            
+            #On crée le formulaire de notation et de commentaires.
+            $form = $app['form.factory']->createBuilder(FormType::class)
+            ->add('commentaires', TextareaType::class , [
+                'required' => false,
+                'label'    => false,
+                'attr' => [
+                    'class'         => 'form-control'
+                ]
+            ])
+            ->add('note', ChoiceType::class, [
+                'required' => false,
+                'label'    => false,
+                'attr' => [
+                    'class'         => 'form-control'
+                ],
+                'choices'  => array(
+                    '*' => 1,
+                    '**' => 2,
+                    '***' => 3,
+                    '****' => 4,
+                    '*****' => 5)
+            ])
+            ->getForm();
+            
+            #Traitement des donneés POST stockées dans $request.
+            $form->handleRequest($request);
+            
+            #Verification de la validité du formulaire.
+            $noteService = $form->getData();
+            $dernierComment = $app['idiorm.db']->for_table('note_services')->where('idService', $idService)->where('idUserNotant', $app['user']->getIdUser())->order_by_desc('dateCommentaire')->limit(1)->find_one();
+            $timeStampActuel = time();
+            $delai = 60*60*24;
+                if($form->isValid())
+                {
+                    if(!empty($noteService) AND (($timeStampActuel - $dernierComment['dateCommentaire']) > $delai))
+                    {
+                        $nouvelleNote = $app['idiorm.db']->for_table('note_services')->create();
+                        #On associe les colonnes de notre BDD avec les valeurs du formulaire
+                        #Colonne MYSQL                                              #Valeurs du Fomulaire
+                        $nouvelleNote->idService             =                          $idService;
+                        $nouvelleNote->idUserNotant          =                          $app['user']->getIdUser();
+                        $nouvelleNote->note                  =                          $noteService['note'];
+                        $nouvelleNote->commentaires          =                          $noteService['commentaires'];
+                        $nouvelleNote->dateCommentaire       =                          time();
+                        
+                        
+                        $nouvelleNote->save();
+                        
+                       
+                         return $app->redirect($app['url_generator']->generate('index_annonce',
+                         [
+                           'idService'              =>                   $idService,
+                           'nomCategorieService'    =>                   ucfirst($nomCategorieService),
+                          'slugService'            =>                   $slugService
+                        ]
+                             ).'?note=success');
+                         
+                    }
+                    
+                    else
+                    {
+                        return $app->redirect($app['url_generator']->generate('index_annonce',
+                            [
+                                'idService'              =>                   $idService,
+                                'nomCategorieService'    =>                   ucfirst($nomCategorieService),
+                                'slugService'            =>                   $slugService
+                            ]
+                            ).'?note=error');
+                    }    
+    //                 return $app['twig']->render('annonce.html.twig', [
+    //                     'service' => $service,
+    //                     'suggestions' => $suggestions,
+    //                     'latitude' => $latitude,
+    //                     'longitude' => $longitude,
+    //                     'form' => $form->createView()
+    //                 ]);
+                
+            }
+            
 
         # Transmission à la Vue
+        # On recupere les notes liés à l'annonce.
+        $noteMoyenne = $app['idiorm.db']->for_table('note_services')->where('idService', $idService)->avg('note');
+        $commentairesService = $app['idiorm.db']->for_table('vue_commentaires_services')->where('idService', $idService)->where_not_equal('commentaires','')->find_result_set();
+        $totalNote = $app['idiorm.db']->for_table('note_services')->where('idService', $idService)->count('note');
+        $nombreStars = round($noteMoyenne, 0, PHP_ROUND_HALF_DOWN);
+        if(($noteMoyenne-$nombreStars) > 0.25)
+        {
+            $halfstar = 'Halfstar';
+        }
+        else
+        {
+            $halfstar = '';
+        }
+        
         return $app['twig']->render('annonce.html.twig', [
             'service' => $service,
             'suggestions' => $suggestions,
             'latitude' => $latitude,
-            'longitude' => $longitude
+            'longitude' => $longitude,
+            'nombreStars' => $nombreStars,
+            'commentairesService' => $commentairesService,
+            'totalNote' => $totalNote,
+            'halfstar' => $halfstar,
+            
+            'form' => $form->createView()
         ]);
     }
 
